c8588bee12560d72d01e12dfbd881d27cc560ceb
Grant <grant.sweiss@gmail.com>
2015-08-18 00:37:32 -0500

    tropical drink link_to

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   get 'about' => 'welcome#about'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
