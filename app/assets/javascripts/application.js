cdcdbddc22b85a9352ffd74d0f976e73f249e394
SirJustinWhite <Jjustinwhite@mac.com>
2015-08-21 12:40:47 -0500

    js refactor, fixed delay from file playback due to loading assets on click from AWS by loading assets first on page load, dynamic file counts so we dont have to change vars every time we add new sound assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,12 @@
 
 $(document).ready(function (event){
 
+	var fileCount = $('.audio-play').length; 
+	//preload all audio assets
+	for (i=0; i < fileCount; i++) {
+		$('.audio-play')[i].load();
+	}
+
 	function mouthDown(distance, time){
 	  $("#mouth").animate({"bottom": ("+=-" + distance + "px")}, time);
 	}
@@ -25,54 +31,30 @@ $(document).ready(function (event){
 		$("#mouth").animate({"bottom": "+=" + distance + "px"}, time);
 	}
 
+	var randomNum = 0;
+	var oldRandomNum = 0;
 	$('.talkingHead').on('click', function (event){
-
-
+		var min = 0; 
+		var max = fileCount-1; // subtract 1 from filecount because arrays start at 0
+	    oldRandomNum = randomNum; //set an oldRandomNum var before resetting in the next line
+	    randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+		
 		function moveMouth(times){
 			var count = 0;
-			var audioCount = 8; //number of audio clips in the welcome#index
-			if (count === 0){
-
-				$('.audio-play')[0].currentTime = 0;
-				$('.audio-play')[(Math.floor(Math.random() * 8))].play(); 
-			}
 			while (count < times){
 				$.when(mouthDown(50, 300)).then(mouthUp(50, 300));
+				$('.audio-play')[oldRandomNum].pause();
+				$('.audio-play')[randomNum].currentTime = 0;
+				$('.audio-play')[randomNum].play();
 				count++;
 			}
 		}
 		moveMouth(3);
 	});
 
+	//small mouth movement when page loads
 	setTimeout(function(){
 		$.when(mouthDown(10, 300)).then(mouthUp(10, 300));
 	}, 400);	
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
