8a8f673e9abbe85a27e816df5bc8373dced5b009
SirJustinWhite <Jjustinwhite@mac.com>
2015-08-21 10:02:00 -0500

    sources audio assets from AWS instead of rails assetpipeline to fix safari playback bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
+  	@baseURL = "https://s3-us-west-2.amazonaws.com/juicebox.xyz/audios/"
   	@audioList = ["FlipFlap1.mp3", "FlipFlap2.mp3", "FlipFlap3.mp3", "FlipFlap4.mp3",
   				  "FlipFlap5.mp3", "FlipFlap6.mp3", "FlipFlap7.mp3", "FlipFlap8.mp3"]
+
   end
 
   def about
